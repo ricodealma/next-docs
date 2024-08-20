c2cde99daa18d671a1c1996ff5f1be7491a9e090
Rafael Henrique dos Reis Santos <rafael.santos@grupomulti.com.br>
2024-08-20 08:30:30 -0300

    bugfix: removing unused import

--- src/server/domain/aggregates/link/ILinkService.ts
+++ src/server/domain/aggregates/link/ILinkService.ts
@@ -1,5 +1,4 @@
 import { Link } from "@/domain/aggregates/link/entities/link";
-import { Result } from "@/domain/seedWork/http/result";
 
 export interface ILinkService {
   createLink(link: Link): Promise<Link>;
